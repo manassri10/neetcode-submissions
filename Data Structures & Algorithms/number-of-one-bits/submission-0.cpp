f115f79b53d25263d78f6876dd923d2881aa9464
Manas Srivastava <manas2120519@gmail.com>
2026-07-02 20:23:51 +0530

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+
+        int count = 0 ; 
+        uint32_t  mask = 1;
+        
+        while(n)
+        {
+            if(n & mask)
+            {
+                count = count + 1;
+            }
+            n = n >> 1;
+        } 
+        return count;  
+    }
+};
